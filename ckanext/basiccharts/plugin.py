41ae296879256c01e6ec974f016b751531ca7732
Vitor Baptista <vitor@vitorbaptista.com>
2014-01-21 16:49:44 +0100

    First prototype drawing a linechart

--- ckanext/basiccharts/plugin.py
+++ ckanext/basiccharts/plugin.py
@@ -1,5 +1,7 @@
 import ckan.plugins as p
 
+not_empty = p.toolkit.get_validator('not_empty')
+
 
 class BasicCharts(p.SingletonPlugin):
     '''This extension makes basic charts'''
@@ -13,16 +15,52 @@ class BasicCharts(p.SingletonPlugin):
         p.toolkit.add_resource('theme/public', 'basiccharts')
 
     def info(self):
-        return {'iframed': False}
+        schema = {
+            'xAxis': [not_empty],
+            'yAxis': [not_empty]
+        }
+
+        return {'name': 'basiccharts',
+                'title': 'Basic Charts',
+                'icon': 'bar-chart',
+                'schema': schema,
+                'iframed': False}
 
     def can_view(self, data_dict):
         return data_dict['resource'].get('datastore_active', False)
 
     def setup_template_variables(self, context, data_dict):
-        return {}
+        fields = _get_fields(data_dict['resource'])
+        xAxis = data_dict['resource_view'].get('xAxis')
+        yAxis = data_dict['resource_view'].get('yAxis')
+        fields = [{'value': v['id']} for v in fields if v['id'] != '_id']
+        return {'fields': fields,
+                'xAxis': xAxis,
+                'yAxis': yAxis,
+                'records': _get_records_from_datastore(data_dict['resource'])}
 
     def view_template(self, context, data_dict):
         return 'basiccharts_view.html'
 
     def form_template(self, context, data_dict):
         return 'basiccharts_form.html'
+
+
+def _get_fields(resource):
+    limit = 0
+    return _datastore_search(resource, limit)['fields']
+
+def _get_records_from_datastore(resource):
+    limit = 5000
+    offset = 0
+    return _datastore_search(resource, limit, offset)['records']
+
+def _datastore_search(resource, limit=None, offset=None):
+    data = {'resource_id': resource['id']}
+    if limit:
+        data['limit'] = limit
+    if offset:
+        data['offset'] = offset
+    result = p.toolkit.get_action('datastore_search')({}, data)
+    return result
+
--- ckanext/basiccharts/tests/test_view.py
+++ ckanext/basiccharts/tests/test_view.py
@@ -1,5 +1,6 @@
 import os
 import inspect
+import mock
 import pylons.config as config
 
 import ckan.plugins as p
@@ -26,3 +27,83 @@ class TestBasicCharts(object):
     def test_plugin_isnt_iframed(self):
         iframed = self.plugin.info().get('iframed', True)
         assert not iframed, 'Plugin should not be iframed'
+
+    def test_can_view_only_if_datastore_is_active(self):
+        active_datastore_data_dict = {
+            'resource': { 'datastore_active': True }
+        }
+        inactive_datastore_data_dict = {
+            'resource': { 'datastore_active': False }
+        }
+        assert self.plugin.can_view(active_datastore_data_dict)
+        assert not self.plugin.can_view(inactive_datastore_data_dict)
+
+    def test_schema_exists(self):
+        schema = self.plugin.info().get('schema')
+        assert schema is not None, 'Plugin should define schema'
+
+    def test_schema_has_xAxis_and_yAxis(self):
+        schema = self.plugin.info()['schema']
+        assert schema.get('xAxis') is not None, 'Scheme should define "xAxis"'
+        assert schema.get('yAxis') is not None, 'Scheme should define "yAxis"'
+
+    def test_schema_xAxis_and_yAxis_are_required(self):
+        schema = self.plugin.info()['schema']
+        not_empty = p.toolkit.get_validator('not_empty')
+        assert not_empty in schema['xAxis'], '"xAxis" should be required'
+        assert not_empty in schema['yAxis'], '"yAxis" should be required'
+
+    @mock.patch('ckan.plugins.toolkit.get_action')
+    def test_setup_template_variables_adds_xAxis_and_yAxis(self, _):
+        resource_view = {
+          'xAxis': 'theXAxis',
+          'yAxis': 'theYAxis'
+        }
+
+        template_variables = self._setup_template_variables('resource_id',
+                resource_view)
+
+        xAxis = template_variables.get('xAxis')
+        yAxis = template_variables.get('yAxis')
+        assert xAxis == 'theXAxis'
+        assert yAxis == 'theYAxis'
+
+    @mock.patch('ckan.plugins.toolkit.get_action')
+    def test_setup_template_variables_adds_fields_without_the_id(self, get_action):
+        fields = [
+          {'id': '_id', 'type': 'int4'},
+          {'id': 'price', 'type': 'numeric'},
+        ]
+        expected_fields = [{'value': 'price'}]
+
+        get_action.return_value.return_value = {
+          'fields': fields,
+          'records': {}
+        }
+        template_variables = self._setup_template_variables()
+
+        returned_fields = template_variables.get('fields')
+        assert returned_fields is not None
+        assert returned_fields == expected_fields
+
+    @mock.patch('ckan.plugins.toolkit.get_action')
+    def test_setup_template_variables_adds_records(self, get_action):
+        records = ['the', 'records']
+
+        get_action.return_value.return_value = {
+          'fields': {},
+          'records': records
+        }
+        template_variables = self._setup_template_variables()
+
+        returned_records = template_variables.get('records')
+        assert returned_records is not None
+        assert returned_records == records
+
+    def _setup_template_variables(self, resource_id='id', resource_view={}):
+        context = {}
+        data_dict = {
+            'resource': { 'id': resource_id },
+            'resource_view': resource_view
+        }
+        return self.plugin.setup_template_variables(context, data_dict)
