29bdb53debb14e83662cf1888826f85b2d2a3fd2
Vitor Baptista <vitor@vitorbaptista.com>
2014-01-24 13:19:44 +0100

    Remove inline javascript

---
+++ ckanext/basiccharts/theme/public/basiccharts.js
@@ -0,0 +1,96 @@
+this.ckan = this.ckan || {};
+this.ckan.views = this.ckan.views || {};
+this.ckan.views.basiccharts = this.ckan.views.basiccharts || {};
+
+(function(self, $) {
+  var resource,
+      resourceView,
+      parsers = {
+        integer: parseInt,
+        numeric: parseFloat,
+        text: function (x) {
+          return x;
+        },
+        timestamp: function (x) {
+          return new Date(x).getTime();
+        }
+      }
+
+  self.init = function init(elementId, _resource, _resourceView) {
+    resource = _resource;
+    resourceView = _resourceView;
+    initPlot(elementId);
+  }
+
+  function initPlot(elementId) {
+    var sql = generateSqlQuery();
+    $.when(
+      recline.Backend.Ckan.fetch(resource),
+      recline.Backend.Ckan.search_sql(sql, resource)
+    ).done(function(fetch, query) {
+      var fields = groupByFieldType(fetch.fields),
+          data = prepareDataForPlot(fields, query.hits),
+          config = plotConfig(fields);
+      $.plot(elementId, data, config);
+    });
+  }
+
+  function generateSqlQuery() {
+    var sql = "SELECT * FROM \""+resource.id+"\"",
+        filterField = resourceView.filter_field,
+        filterValue = resourceView.filter_value;
+
+    if (filterField && filterValue) {
+      sql += " WHERE "+filterField+" = '"+filterValue+"'";
+    }
+
+    return sql;
+  }
+
+  function groupByFieldType(fields) {
+    var result = {};
+    $.each(fields, function (i, field) {
+      result[field.id] = field.type;
+    });
+    return result;
+  }
+
+  function prepareDataForPlot(fields, records) {
+    var grouppedData = convertAndGroupDataBySeries(fields, records),
+        data = $.map(grouppedData, function(data, label) {
+      return {
+        label: label,
+        data: data
+      }
+    });
+
+    return data;
+  }
+
+  function plotConfig(fields) {
+    var config = {},
+        xAxisType = fields[resourceView.xAxis],
+        yAxisType = fields[resourceView.yAxis];
+
+    if (xAxisType === "timestamp") {
+      config.xaxis = { mode: "time" }
+    }
+    if (yAxisType === "timestamp") {
+      config.yaxis = { mode: "time" }
+    }
+    return config;
+  }
+
+  function convertAndGroupDataBySeries(fields, records) {
+    var result = {};
+    $.each(records, function(i, record) {
+      var x = parsers[fields[resourceView.xAxis]](record[resourceView.xAxis]),
+          y = parsers[fields[resourceView.yAxis]](record[resourceView.yAxis]),
+          series = record[resourceView.series];
+
+      result[series] = result[series] || []
+      result[series].push([x, y])
+    });
+    return result;
+  }
+})(this.ckan.views.basiccharts, this.jQuery);
