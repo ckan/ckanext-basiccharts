d9a360b7b8867bdd1facb84b7d597eb41e244a0b
Vitor Baptista <vitor@vitorbaptista.com>
2014-02-10 11:12:25 -0300

    We don't need to implement IPackageController

--- ckanext/basiccharts/plugin.py
+++ ckanext/basiccharts/plugin.py
@@ -19,7 +19,6 @@ class BaseChart(p.SingletonPlugin):
 
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IResourceView, inherit=True)
-    p.implements(p.IPackageController, inherit=True)
 
     def update_config(self, config):
         p.toolkit.add_template_directory(config, 'theme/templates')
