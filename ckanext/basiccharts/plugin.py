6c4a4d99416d9b233e8e81bb5c322ec516fa6bee
kindly <kindly@gmail.com>
2014-03-14 17:25:28 +0000

    Adds dropdown list for filter values when there is a small amount of data

--- ckanext/basiccharts/plugin.py
+++ ckanext/basiccharts/plugin.py
@@ -9,7 +9,8 @@ class BasicCharts(p.SingletonPlugin):
     p.implements(p.ITemplateHelpers)
 
     def get_helpers(self):
-        return {'remove_linebreaks': _remove_linebreaks}
+        return {'remove_linebreaks': _remove_linebreaks,
+                'get_filter_values': get_filter_values}
 
 
 class BaseChart(p.SingletonPlugin):
@@ -150,3 +151,33 @@ def _get_fields(resource):
 def _remove_linebreaks(string):
     '''Convert a string to be usable in JavaScript'''
     return str(string).replace('\n', '')
+
+def get_filter_values(resource):
+    ''' Tries to get out filter values so they can appear in dropdown list.
+    Leaves input as text box when the table is too big or there are too many
+    distinct values.  Current limits are 5000 rows in table and 500 distict
+    values.'''
+
+    data = {
+        'resource_id': resource['id'],
+        'limit': 5001
+    }
+    result = p.toolkit.get_action('datastore_search')({}, data)
+    # do not try to get filter values if there are too many rows.
+    if len(result.get('records', [])) == 5001:
+        return {}
+
+    filter_values = {}
+    for field in result.get('fields', []):
+        if field['type'] != 'text':
+            continue
+        distinct_values = set()
+        for row in result.get('records', []):
+            distinct_values.add(row[field['id']])
+        # keep as input if there are too many distinct values.
+        if len(distinct_values) > 500:
+            continue
+        filter_values[field['id']] = [{'id': value, 'text': value}
+                                      for value
+                                      in sorted(list(distinct_values))]
+    return filter_values
--- ckanext/basiccharts/theme/public/basiccharts_form.js
+++ ckanext/basiccharts/theme/public/basiccharts_form.js
@@ -1,21 +1,59 @@
 ckan.module("basiccharts_filters_form", function (jQuery) {
   "use strict";
 
+  function applyDropdown(selectField, filterValues) {
+    var self = this,
+        inputField = selectField.parent().find('input'),
+        input_value = inputField.val(),
+        valueList;
+
+    valueList = filterValues[selectField.val()];
+    if (valueList) {
+      inputField.replaceWith('<input type="hidden" value="" name="filter_values">');
+      inputField = selectField.parent().find('input'),
+      inputField.val(input_value);
+      inputField.select2({
+          data: valueList,
+          width: '220px'
+      })
+    }
+  }
+
   function initialize() {
     var self = this,
         templateFilterInputs = self.options.templateFilterInputs,
         filtersDiv = self.el.find(self.options.filtersSelector),
         addFilterEl = self.el.find(self.options.addFilterSelector),
-        removeFilterSelector = self.options.removeFilterSelector;
+        removeFilterSelector = self.options.removeFilterSelector,
+        filterValues = self.options.filterValues;
+
+    var selects = filtersDiv.find('select');
+    selects.each(function (i, select) {
+       applyDropdown($(select), filterValues);
+    })
 
     addFilterEl.click(function (evt) {
+      var inputField, selectField, valueList, input_value
       evt.preventDefault();
       filtersDiv.append(templateFilterInputs);
+      selectField = filtersDiv.children().last().find('select');
+      applyDropdown(selectField, filterValues)
     });
+
 
     filtersDiv.on("click", removeFilterSelector, function (evt) {
       evt.preventDefault();
       $(this).parent().remove();
+    });
+
+    filtersDiv.on("change", 'select', function (evt) {
+      var inputField = $(this).parent().find('input');
+      var select2Contianer = $(this).parent().find('.select2-container');
+      evt.preventDefault();
+      select2Contianer.remove();
+      inputField.replaceWith('<input type="text" value="" name="filter_values">');
+      inputField = $(this).parent().find('input');
+      applyDropdown($(this), filterValues);
     });
   }
 
