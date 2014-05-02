9ef8ee711d3f37236f25e86aa6ce0670c1120d84
Vitor Baptista <vitor@vitorbaptista.com>
2014-05-02 09:21:14 -0300

    Change color schema
    There used to be a color almost white, which looks terrible on a white
    background. I've changed our schema to a 9-class colorblind safe one from
    http://colorbrewer2.org/

--- ckanext/basiccharts/theme/public/basiccharts.js
+++ ckanext/basiccharts/theme/public/basiccharts.js
@@ -121,7 +121,10 @@ this.ckan.views.basiccharts = this.ckan.views.basiccharts || {};
         };
 
     config = {
-      yaxis: axisConfigByType[yAxisType]
+      yaxis: axisConfigByType[yAxisType],
+      colors: ['#e41a1c', '#377eb8', '#4daf4a',
+               '#984ea3', '#ff7f00', '#ffff33',
+               '#a65628', '#f781bf', '#999999']
     };
 
     if (params.chart_type == "pie") {
